a4afa3f380554f77e8a9616bda2a7d1c785725b1
Chris Ortman <chrisortman@gmail.com>
2017-09-19 13:17:44 +0000

    Address Program

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,19 +6,19 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Sophie Waterman Hines")
 
 # display the address
-
+print("742 Evergreen Terrace")
 
 # display the city, state and zip
-
+print("Iowa City, Iowa, 45567")
 
 # display the phone number
-
+print("123-456-7890")
 
 # display the course area
-
+print("Computer Science 101")
 
 
 
