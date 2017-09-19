38f005f37b5f80b2d37487d0cb0cc519f022d02d
Chris Ortman <chrisortman@gmail.com>
2017-09-19 13:42:45 +0000

    Lookin' good.

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,19 +6,21 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-print("Sophie Waterman Hines")
+name = "Sophie Waterman Hines"
 
 # display the address
-print("742 Evergreen Terrace")
+address = "742 Evergreen Terrace"
 
 # display the city, state and zip
-print("Iowa City, Iowa, 45567")
+city = "Iowa City, Iowa, 45567"
 
 # display the phone number
-print("123-456-7890")
+phone = "123-456-7890"
 
 # display the course area
-print("Computer Science 101")
+course = "Computer Science 101"
+
+print (name, address, city, phone, course)
 
 
 
